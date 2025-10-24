4798d6e309fe1bce6c18a7c68935e1e8b45f7673
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2025-10-24 15:30:59 +0200

    added sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let higher = 0
+    let lower = 0
+    if (a === b || a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR'
+    }
+    if (a > b) {
+        higher = a
+        lower = b
+    }
+    if (a < b) {
+        higher = b
+        lower = a
+    }
+    let result = higher
+    for (i = higher - 1; i >= lower; i--)
+    {
+        result += i
+    }
+    return result
 };
 
 // Do not edit below this line
