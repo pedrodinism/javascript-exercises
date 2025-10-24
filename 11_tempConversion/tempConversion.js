d388174fd480aeb154a81279e36c167fa4d07923
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2025-10-24 15:42:08 +0200

    added convertToCelsius and convertToFahrenheit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+  let tempCelsius = (tempFah - 32) * 5/9
+  return Math.round(tempCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFah = tempCelsius * 9/5 + 32
+  return Math.round(tempFah * 10) / 10
 };
 
 // Do not edit below this line
