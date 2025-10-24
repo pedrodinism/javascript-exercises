c202de3784466f7c32c2b7bf77c0d00de5ec16a5
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2025-10-24 15:31:31 +0200

    added removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, ...args) {
+    const toRemove = new Set(args)
+    return array.filter(item => !toRemove.has(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
